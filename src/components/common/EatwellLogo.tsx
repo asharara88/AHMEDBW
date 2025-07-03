ff085411c7f18d400e86673a461418b86558ad83
asharara88 <ahmed.m.sharara@gmail.com>
2025-07-03 10:02:47 +0400

    Increase EATWELL and MOVEWELL logo sizes

--- src/components/common/EatwellLogo.tsx
+++ src/components/common/EatwellLogo.tsx
@@ -12,7 +12,7 @@ const EatwellLogo: React.FC<EatwellLogoProps> = ({
       <img 
         src="https://leznzqfezoofngumpiqf.supabase.co/storage/v1/object/sign/logos/3.svg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV82ZjcyOGVhMS1jMTdjLTQ2MTYtOWFlYS1mZmI3MmEyM2U5Y2EiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJsb2dvcy8zLnN2ZyIsImlhdCI6MTc1MTQ1MjY3OSwiZXhwIjoxNzgyOTg4Njc5fQ.ooTqlo57-N42aRcfvvbvC9bE9C3mKp4rdWwC2u5ZzMo" 
         alt="EATWELL" 
-        className="h-6 w-auto"
+        className="h-24 w-auto" // Increased from h-6 to h-24 (4x size)
       />
     </div>
   );
--- src/components/common/MovewellLogo.tsx
+++ src/components/common/MovewellLogo.tsx
@@ -12,7 +12,7 @@ const MovewellLogo: React.FC<MovewellLogoProps> = ({
       <img 
         src="https://leznzqfezoofngumpiqf.supabase.co/storage/v1/object/sign/logos/2.svg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV82ZjcyOGVhMS1jMTdjLTQ2MTYtOWFlYS1mZmI3MmEyM2U5Y2EiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJsb2dvcy8yLnN2ZyIsImlhdCI6MTc1MTQ1MjcwMCwiZXhwIjoxNzgyOTg4NzAwfQ.ToGkjAJv89sLydmj8qQwVXYrKjvnPEU_zGWhAmVo1yE" 
         alt="MOVEWELL" 
-        className="h-6 w-auto"
+        className="h-24 w-auto" // Increased from h-6 to h-24 (4x size)
       />
     </div>
   );
