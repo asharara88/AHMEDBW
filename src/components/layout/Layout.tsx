ab5c5db84b760f4b482f38e16473e293a8a02f54
asharara88 <ahmed.m.sharara@gmail.com>
2025-07-05 14:39:15 +0400

    Fix Layout component to properly render children

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 // This file should contain layout components, not page components
-// The FitnessPage component has been moved to src/pages/fitness/FitnessPage.tsx
-// where it belongs and is properly wrapped with AuthProvider
 
-export default function Layout() {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      {/* Layout components should go here */}
+      {children}
     </div>
   );
 }
