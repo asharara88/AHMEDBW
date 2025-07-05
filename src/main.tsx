09e8cced37f393f6d74b1eee15ac5d22de497cc5
asharara88 <ahmed.m.sharara@gmail.com>
2025-07-05 14:52:28 +0400

    Add SupabaseProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { SupabaseProvider } from './contexts/SupabaseContext';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </AuthProvider>
+      <SupabaseProvider>
+        <AuthProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </AuthProvider>
+      </SupabaseProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
